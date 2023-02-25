26c678050946939dc75ef474d330ebd586cbdcc3
akhilsai0099 <akhilsai0099@gmail.com>
2023-02-25 08:33:32 +0530

    ui changes for task complete

--- app.js
+++
@@ -1,7 +0,0 @@
-let a = JSON.stringify("oeuaoueu","thosue")
-let arr = [{task:"hello",completed:false},{
-  task:"hii",completed:false
-}]
-let b = JSON.stringify([...arr,{task:"hey",completed:false}])
-let parsed = JSON.parse(b)
-console.log(parsed)
